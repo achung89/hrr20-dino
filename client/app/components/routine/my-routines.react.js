a53ec6c8372ef9902e4d7aa47b7545b81dcaf62b
Jordan Scholten <schojo13@gmail.com>
2016-12-15 13:41:32 -0800

    Plan for implementing state change

--- client/app/components/routine/my-routines.react.js
+++ client/app/components/routine/my-routines.react.js
@@ -7,9 +7,9 @@ import Launch from 'material-ui/svg-icons/action/launch';
 import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
+import Checkbox from 'material-ui/Checkbox';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import { Link } from 'react-router';
-import Checkbox from 'material-ui/Checkbox';
 
 // flux
 import RoutineStore from '../../flux/stores/routine-store';
@@ -26,7 +26,8 @@ export default class MyRoutines extends React.Component {
 
     this.state = {
       routines: [],
-      tasks: []
+      tasks: [],
+
     };
   }
 
@@ -73,16 +74,15 @@ export default class MyRoutines extends React.Component {
     RoutineActions.remove(id);
   }
 
-handleTaskLineThrough(e) {
-  console.log(this.props, this, e); //this = myRoutines
-  // this.props.style={{textDecoration: 'none'}}
-  // toggle ListItem's style prop between {textDecoration: 'none'} and {textDecoration: 'line-through'}
-    // if(target.style.textDecoration === 'none') {
-    // target.style.textDecoration = 'line-through'
+  handleToggleCheckbox(e) {
+    console.log('YO', this, e)
+    // swap state at passed in id
+    // if(state.toggle[id].textDecoration === 'none') {
+    //  state.toggle[id].textDecoration = 'line-through'
     // } else {
-    // target.style.textDecoration = 'none'
+    //  state.toggle[id].textDecoration = 'none'
     // }
-}
+  };
 
   render() {
     const paperStyle = {
@@ -118,9 +118,8 @@ handleTaskLineThrough(e) {
                       {/* insert onTapTouch for ListItem */}
                       <ListItem
                         primaryText={task.name}
-                        style={{textDecoration: 'line-through'}}
-                        leftCheckbox={<Checkbox/>}
-                        onChange={this.handleTaskLineThrough.bind(this)}
+                        leftCheckbox={<Checkbox />}
+                        onChange={this.handleToggleCheckbox.bind(this)}
                         rightIcon={<Link params={{ name: routine.name }} to={`/tasks/${task.name}`}><Launch /></Link>}
                       >
                       </ListItem>
